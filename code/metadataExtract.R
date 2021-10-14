8e23461996446b5913a05b790cc133a39898d6ef
kaclaborn <clabornkelly@gmail.com>
2021-10-14 15:43:05 -0700

    update metadataExtract.R script for initial pre-processing prior to analysis

--- code/metadataExtract.R
+++ code/metadataExtract.R
@@ -7,40 +7,54 @@
 # 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
-# ---- SECTION 1: IMPORT LIBRARIES & CORPUS ----
+# ---- SECTION 1: IMPORT TEXT FILES & EXTRACT METADATA ----
 #
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
 
-pacman::p_load()
 
-
+# List files from Alaska Dispatch News (ADN) to be included in corpus (n = 1429)
 ADNfiles <- 
   paste('data/corpus/post-processed/', list.files('data/corpus/post-processed'), sep = "")
-  
 
-  
-# 
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#
-# ---- SECTION 2: EXTRACT METADATA FROM TXT FILES ----
-#
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-#
-
-
+# Create empty vectors to append full text and metadata to
 fullstring <- c()
 year <- c()
 intro <- c()
 
-
+# Run loop to import full text and metadata for each file listed in ADNfiles
 for(i in 1:length(ADNfiles)) {
   fullstring <- append(fullstring, readLines(ADNfiles[i]))
   year <- append(year, sub(".*Copyright (\\d{4}).*", "\\1", fullstring[i]))
   intro <- append(intro, sub(" Copyright.*", "\\1", fullstring[i]))
 }
 
-ADNcorpus <- cbind.data.frame(source = "Alaska Dispatch News",
-                              year = year,
-                              intro = intro,
-                              fullstring = fullstring)
+# Bind the vectors back together into a new data frame
+ADN <- cbind.data.frame(doc_id = c(1:length(fullstring)),
+                        source = "Alaska Dispatch News",
+                        year = year,
+                        intro = intro,
+                        text = fullstring)
+
+# Create corpus using quanteda package
+ADNcorpus <- quanteda::corpus(ADN)
+
+  
+# 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# ---- SECTION 2: IMPORT LEMMAS AND STOPWORDS ----
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+
+
+# Lemma data file 
+lemma_data <- read.csv('data/resources/baseform_en.tsv', encoding = 'UTF-8', sep = "\t", header = F) %>%
+  rename("inflected_form" = "V1",
+         "lemma" = "V2") %>%
+  na.omit()
+
+# Stopwords file
+stopwords_extended <- readLines('data/resources/stopwords_en.txt', encoding = 'UTF-8')
+
