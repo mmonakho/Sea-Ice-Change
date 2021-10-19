8f718501121afff8af1455b609587fc4c6da0538
Masha Monakhova (Student) <mmonakho@sundevils.asu.edu>
2021-10-19 10:23:02 -0700

    Old tutorial

--- code/Masha1.R
+++ code/Masha1.R
@@ -30,17 +30,40 @@ txtfiles <- paste('data/corpus/post-processed/',
 
 # ---- 1.2 Bi-grams ----
 
+a1 <- readLines('data/corpus/post-processed/text1.txt', skipNul=TRUE)
+
+a_list <- list.files('data/corpus/post-processed', full.names = TRUE)
+loop <- lapply(a_list, readLines, skipNul = TRUE)
+
+new_corpus <- as.VCorpus(loop)
+
+# article <- new_corpus[[1]]
+
 # read in text
-newspapers <- base::readLines(txtfiles[1]) %>%
+newspapers <- base::readLines(new_corpus) %>%
   paste0(collapse = " ") %>%
   stringr::str_squish() %>%
   stringr::str_remove_all("- ")
+
 # further processing
 newspapers_split <- newspapers %>% 
   as_tibble() %>%
   tidytext::unnest_tokens(words, value)
+
 # create data frame
 newspapers_words <- newspapers_split %>%
   dplyr::rename(word1 = words) %>%
   dplyr::mutate(word2 = c(word1[2:length(word1)], NA)) %>%
   na.omit()
+
+# inspect the frequency of each bigram
+newspapers2grams <- newspapers_words %>%
+  dplyr::mutate(bigram = paste(word1, word2, sep = " ")) %>%
+  dplyr::group_by(bigram) %>%
+  dplyr::summarise(frequency = n()) %>%
+  dplyr::arrange(-frequency)
+
+# clean bigram table
+stps <- paste0(tm::stopwords(kind = "en"), collapse = "\\b|\\b")
+newspapers2grams_clean <- newspapers2grams %>%
+  dplyr::filter(!str_detect(bigram, stps))
