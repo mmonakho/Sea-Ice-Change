8a5936ec7609cc2c144f7e695e927a0fb745bd5a
Masha Monakhova (Student) <mmonakho@sundevils.asu.edu>
2021-10-26 10:57:44 -0700

    Added sections

--- code/Topic_Modelling.R
+++ code/Topic_Modelling.R
@@ -1,10 +1,30 @@
+# 
+# code: Topic Modelling
+# 
+# author: Masha Monakhova, mmonakho@asu.edu.com
+# date: October 2021
+# 
+# 
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# ---- SECTION 1: LOAD THE DATA & PACKAGES ----
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 options(stringsAsFactors = FALSE)
 
 pacman::p_load(topicmodels, quanteda, quanteda.textstats)
 source('code/metadataExtract.R')
 
-# Create a DTM (may take a while)
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+# ---- SECTION 2: CREATE A DTM AND COMPUTE THE LDA MODEL ----
+#
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+# ---- 1.1 Create DTM ----
+
 corpus_tokens <- ADNcorpus %>% 
   tokens(remove_punct = TRUE, remove_numbers = TRUE, remove_symbols = TRUE) %>% 
   tokens_tolower() %>% 
@@ -16,7 +36,7 @@ ADNcollocations <- ADNcollocations[1:250]
 
 corpus_tokens <- tokens_compound(corpus_tokens, sotu_collocations)
 
-# Create DTM, but remove terms which occur in less than 1% of all documents
+# ---- 1.2 Remove terms which occur in less than 1% of all documents
 DTM <- corpus_tokens %>% 
   tokens_remove("") %>%
   dfm() %>% 
@@ -27,9 +47,11 @@ DTM <- DTM[sel_idx, ]
 ADN <- ADN[sel_idx, ]
 
 require(topicmodels)
-# number of topics
-K <- 10
-# compute the LDA model, inference via n iterations of Gibbs sampling
+
+# ---- 1.3 Choose the number of topics
+K <- 15
+
+# ---- 1.4 Compute the LDA model, inference via n iterations of Gibbs sampling
 topicModel <- LDA(DTM, K, method="Gibbs", control=list(iter = 500, seed = 1, verbose = 25))
 
 terms(topicModel, 10)
